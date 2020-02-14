98482d973dcec57fa1142f10b33bc6ea48a82eca
Brita Hill <bhill@Britas-MacBook-Pro.local>
2020-02-13 16:40:07 -0800

    Added function that talks to chained motors

---
+++ Arduino_files/base_pos_func.m
@@ -0,0 +1,28 @@
+#include <DynamixelWorkbench.h>
+DynamixelWorkbench dxl_wb;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  //set robot to rest position
+  setRestPosition(); 
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+//returns arm to rest position for every chained motor in arm 
+
+void setRestPosition(){
+  dxl_wb.goalPosition(1, (int32_t)100); //motor 1
+  dxl_wb.goalPosition(1, (int32_t)200); //motor 2
+  dxl_wb.goalPosition(1, (int32_t)300); //motor 3
+  dxl_wb.goalPosition(1, (int32_t)400); //motor 4
+  dxl_wb.goalPosition(1, (int32_t)500); 
+  dxl_wb.goalPosition(1, (int32_t)600); 
+  delay(3000);
+}
