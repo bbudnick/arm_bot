0db03983761874d471c392aa89a5955f3df316d1
Brita Hill <46576125+bhill56@users.noreply.github.com>
2020-02-28 11:35:20 -0800

    Update setRestPos.ino
    Added functionality to pinch end effector. Next, need to daisy chain motors and change their orientation depending on previous motor.

--- myArduinoFiles/setRestPos.ino
+++ myArduinoFiles/setRestPos.ino
@@ -6,39 +6,68 @@
  * to establish rest position depending on how the 
  * motors are assembled along the arm. 
  * 
- * 
+ * Updated as of Feb 28 to communicate to motors 1 and 2 successively 
+ *
  * 
  */
 
 #include <DynamixelWorkbench.h>
 DynamixelWorkbench dxl_wb;
 
-
 void setup() {
+  uint8_t ID1 = 1; 
+  uint8_t ID2 = 2; 
+  uint8_t ID3 = 3; 
+  uint8_t ID4 = 4; 
+  uint8_t ID5 = 5; 
+  uint8_t ID6 = 6; 
 
-  uint8_t ID = 4; 
-
-  //Initialize communication to the dynamixels
+  //FIRST:
+  //Initialize communication to all 6 of the the dynamixels
   dxl_wb.init("1", 1000000); 
   uint16_t model_number = 0; 
-  dxl_wb.ping(ID, &model_number);
-  
-  // put your setup code here, to run once:
-  uint8_t a = 0; 
-  uint8_t b = 0; 
+  dxl_wb.ping(ID1, &model_number);
+  dxl_wb.ping(ID2, &model_number);
+  dxl_wb.ping(ID3, &model_number);
+  dxl_wb.ping(ID4, &model_number);
+  dxl_wb.ping(ID5, &model_number);
+  dxl_wb.ping(ID6, &model_number);
 
-  //set robot to rest position
-  //setRestPosition(); 
-  dxl_wb.goalPosition(ID, (int32_t)512);
+  //Chain first two motors together to test their angles
+  //TODO CONTINUE TESTING FROM HERE
+  uint8_t a = ID1; 
+  uint8_t b = ID6; 
+  int adjustPosition(uint8_t a, uint8_t b);
+
+ 
+  
+  
+  /*
+  end_effector_pinch(ID6, model_number); 
   //Adjusts position of motor 2 based on the position of motor 3
   //int pos2 = adjustPosition(3,2); 
-
+*/
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+//This function can be called from setup() to have the end-effector grip the pen
+//TODO: augment later with a check that will read from the motor to confirm its ID 
+void end_effector_pinch(uint8_t ID, uint16_t &model_number) {
+  if(ID == 6){
+  //function for performing pinching action of end-effector motor
+  dxl_wb.goalPosition(ID, (int32_t)0);
+  delay(1000); 
+  dxl_wb.goalPosition(ID, (int32_t)512);
+  delay(1000);}
+  else {
+    Serial.print("Pinch action could not be performed.");
+    Serial.print("The ID of the motor is not at the end-effector."); 
+  }
 }
 
 //returns arm to rest position for every chained motor in arm 
@@ -48,14 +77,18 @@ void setRestPosition(){
   dxl_wb.goalPosition(2, (int32_t)200); //motor 2
   dxl_wb.goalPosition(3, (int32_t)300); //motor 3
   dxl_wb.goalPosition(4, (int32_t)400); //motor 4
-  dxl_wb.goalPosition(5, (int32_t)500); 
+  dxl_wb.goalPosition(5, (int32_t)500); //motor 5
+  dxl_wb.goalPosition(6, (int32_t)600); //motor 6, end effector
+  //motor 6 centerpoint is 512
+  //motor 6 appears to only be able to move between 0-512 
+
   delay(3000);
 }
 
 //Set posiiton of motor b based on position of motor a 
 //Returns the new value of the position of motor b 
 //TODO Haven't called this yet, not sure what it will do! 
-int adjustPosition(int a, int b)
+int adjustPosition(uint8_t a, uint8_t b)
 {
   int32_t aPos = 0;
   int32_t t023 = 0; 
@@ -75,4 +108,3 @@ int adjustPosition(int a, int b)
     return 1023; 
   }
 }
-
