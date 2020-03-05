e6a1f6a1b4c8edb387f1e1cd37e5785c447c0747
Brita Hill <bhill@Britas-MacBook-Pro.local>
2020-03-05 15:24:22 -0800

    Added master scipt functionality to interact with IKinSpace

--- myMATLABFiles/master_test_script.m
+++ myMATLABFiles/master_test_script.m
@@ -1,16 +1,20 @@
 letter_vectors;
 
 user_input = 'A';
-thetalist = [0; 0; 0; 0]
-Slist = [[0; 0;  1;  4; 0;    0],
-    [0; 0;  0;  0; 1;    0],
-    [0; 0;  0;  1; 0;    0],
-    [0; 0; -1; -6; 0; -0.1]]
+thetalist0 = [1.5; 2.5; 3; 5]; 
+eomg = 0.01; 
+ev = 0.001; 
+Slist = [[0; 0;  1;  4; 0; 0],[0; 0;  0;  0; 1; 0],[0; 0;  0;  1; 0; 0],[0; 0; -1; -6; 0; -0.1]];
 
 %send user input to get appropriate vectors
 stuff = matchLetter(user_input);
 
-make_matrices = usesLinSpace(stuff)
-FKinSpace(make_matrices, Slist, thetalist)
+T = usesLinSpace(stuff)
+for i = 1:size(T,3)
+  internalT = T(:, :, i)
+  [thetalist, success] = IKinSpace(make_matrices, Slist, thetalist)
+end
+
+
 
 
