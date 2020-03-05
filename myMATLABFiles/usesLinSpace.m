67e0894642fba234ac5bbd8d1296f42d5263c670
Brita Hill <bhill@Britas-MacBook-Pro.local>
2020-03-04 23:27:03 -0800

    More changes to usesLinSpace because I have a hard time putting things down. It all seems to work?

--- myMATLABFiles/usesLinSpace.m
+++ myMATLABFiles/usesLinSpace.m
@@ -1,30 +1,31 @@
-% When I test this function line-by-line, it seems to work. 
-% There is something about the way I'm looping that MATLAB doesn't like.
-% Will come back to it 3/5. 
+%Not sure why this isn't totally working, will test more tomorrow with a rested brain. 
+
 
 function a = testUsesLinSpace(current_letter)
-    letter_vectors;
     
     [m,n] = size(current_letter) %m is rows, n is columns
-
+    
     for i=1:n %iterate column by column sending columns two at a time to LinSpace
-        current = current_letter(:, i); %current position vector
-        goal = current_letter(:, i+1); %goal position vector
-        x_res = linspace(current(1, 1), goal(1, 1), 5); %3 intermediate values between current x and goal x
-        y_res = linspace(current(2, 1), goal(2, 1), 5);
-        z_res = linspace(current(3, 1), goal(3, 1), 5);
-        intermediate = [x_res; y_res; z_res]; %make an array of the new intermediate vertices
-        [m,n] = size(intermediate); %should be 3x5 matrix
-        agg_mat = intermediate;
-        tot_mat = horzcat(agg_mat, intermediate); % TODO THIS MIGHT NOT WORK, goal is to concatenate new intermediate matrix to existing matrix of linspace results
+        for j=2:n %the only solution I can think of as a replacement for i+1
+            current = current_letter(:, i); %current position vector
+            goal = current_letter(:, j); %goal position vector
+            x_res = linspace(current(1, 1), goal(1, 1), 5); %3 intermediate values between current x and goal x
+            y_res = linspace(current(2, 1), goal(2, 1), 5);
+            z_res = linspace(current(3, 1), goal(3, 1), 5);
+            intermediate = [x_res; y_res; z_res]; %make an array of the new intermediate vertices
+            [m,n] = size(intermediate); %should be 3x5 matrix
+            agg_mat = intermediate;
+            tot_mat = horzcat(agg_mat, intermediate); % TODO THIS MIGHT NOT WORK, goal is to concatenate new intermediate matrix to existing matrix of linspace results
+        end
     end
-   
+    
     R = [1, 0, 0; 0, 1, 0; 0, 0, 1; 0, 0, 0];
     
+    % Frankenstein T matrices together using R and each column of tot_mat
     [m,n] = size(tot_mat) %m is rows, n is columns, should be 3xN
     for i=1:n
-        super_final_p = cat(1, tot_mat, 1);
-        T = [R, super_final_p(i)]; % 4x4 T matrix
+        cur_col = cat(1, tot_mat(i), 1); 
+        T = horzcat(R, cur_col);
         a = T;
     end
 end
