5204a4eb480cd93225887dcc80e92c167cf8152e
Brita Hill <bhill@Britas-MacBook-Pro.local>
2020-03-04 21:23:10 -0800

    Wrote all of usesLinSpace code which should create a T matrix that is returned and can be passed to FKinSpace

--- myMATLABFiles/usesLinSpace.m
+++ myMATLABFiles/usesLinSpace.m
@@ -1,71 +1,31 @@
+% Complete but untested
 % Using each vector from the chosen letter, 
 % process through linspace to generate increments between each vector.
 % Create T matrices for each increment from linspace to be
 % sent to sent to FKinSpace 
 
-function a = usesLinSpace(thetalist0, user_choice) 
+function a = usesLinSpace(user_choice) 
     
-    transposeLetter = 0; 
+    MONDO = (3*size(user_choice)); %preallocate array to hold intermediate vectors to be created
+    
     transposeLetter = transpose(user_choice); %transpose letter matrix to get 
 
     for P = 1 : size(transposeLetter) %iterate row by row, sending rows two at a time to LinSpace
-        current = transposeLetter(I, :); %current position vector
-        goal = transposeLetter(I+1, :); %goal position vector
-        for Q = 1 : size(current)
-            lin_res = linspace(J, K, 3); %lin_res will hold 3 values between J and K scalars
-        end
-        for R = 1 : size(goal)
-            lin_res = linspace(transpose_letter())
-                
-                    MONDO[Y] = lin_res; 
-                % TODO: save each lin_res array to larger 2d matrix that
-                % will get zombied with R 
-                
-                %I dunno this is a mess 
-        
-            end
-        end
+        current = transposeLetter(P, :); %current position vector
+        goal = transposeLetter(P+1, :); %goal position vector
+        x_res = linspace(current(1), goal(1), 3); %3 intermediate values between current x and goal x
+        y_res = linspace(current(2), goal(2), 3);
+        z_res = linspace(current(3), goal(3), 3);
+        MONDO(P) = [x_res; y_res; z_res]; %should catch all intermediate values 
     end
-
-
-      
-
- 
- 
- 
- 
- %this function takes the initial p_i x, then p_i y, then p_i z
- % 
- % 
- % p_i vector, p_(i+1), and the number of
- %increments to draw between them 
- 
-     
-         
+    final_p = transpose(MONDO); %should be a 3xN matrix holding all x, y, z vals with intermediate values
+    
     R = [1, 0, 0; 0, 1, 0; 0, 0, 1; 0, 0, 0];
     
-    p = [lin_res]
-    
-    
-    initT = [1, 0, 0, 0;
-        0, 1, 0, -1;
-        0, 0, 1, 0; 
-        0, 0, 0, 1]
-    
-    goalT = [1, 0, 0, 1;
-            0, 1, 0, 0; 
-            0, 0, 1, -1; 
-            0, 0, 0, 1];
-    
-
-        
-
-
-    
-    
-    
-    
-    
-    
+    for Q = 1 : size(final_p)
+        super_final_p = cat(1, final_p, 1);
+        T = [R, super_final_p(Q)]; % 4x4 T matrix
+        a = T; 
+    end
 end
 
