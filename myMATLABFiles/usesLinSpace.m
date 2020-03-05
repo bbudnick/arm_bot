4ec6b0a0cb9e1b0be88322d33bec6f9f54228301
Brita Hill <bhill@Britas-MacBook-Pro.local>
2020-03-04 23:06:37 -0800

    Added functionality to usesLinSpace.m after testing line by line. Something about MATLAB syntax with loops isn't working for me.

--- myMATLABFiles/usesLinSpace.m
+++ myMATLABFiles/usesLinSpace.m
@@ -1,31 +1,33 @@
-% Complete but untested
-% Using each vector from the chosen letter, 
-% process through linspace to generate increments between each vector.
-% Create T matrices for each increment from linspace to be
-% sent to sent to FKinSpace 
+% When I test this function line-by-line, it seems to work. 
+% There is something about the way I'm looping that MATLAB doesn't like.
+% Will come back to it 3/5. 
 
-function a = usesLinSpace(user_choice) 
+function a = testUsesLinSpace(current_letter)
+    letter_vectors;
     
-    MONDO = (3*size(user_choice)); %preallocate array to hold intermediate vectors to be created
-    
-    transposeLetter = transpose(user_choice); %transpose letter matrix to get 
+    [m,n] = size(current_letter) %m is rows, n is columns
 
-    for P = 1 : size(transposeLetter) %iterate row by row, sending rows two at a time to LinSpace
-        current = transposeLetter(P, :); %current position vector
-        goal = transposeLetter(P+1, :); %goal position vector
-        x_res = linspace(current(1), goal(1), 3); %3 intermediate values between current x and goal x
-        y_res = linspace(current(2), goal(2), 3);
-        z_res = linspace(current(3), goal(3), 3);
-        MONDO(P) = [x_res; y_res; z_res]; %should catch all intermediate values 
+    for i=1:n %iterate column by column sending columns two at a time to LinSpace
+        current = current_letter(:, i); %current position vector
+        goal = current_letter(:, i+1); %goal position vector
+        x_res = linspace(current(1, 1), goal(1, 1), 5); %3 intermediate values between current x and goal x
+        y_res = linspace(current(2, 1), goal(2, 1), 5);
+        z_res = linspace(current(3, 1), goal(3, 1), 5);
+        intermediate = [x_res; y_res; z_res]; %make an array of the new intermediate vertices
+        [m,n] = size(intermediate); %should be 3x5 matrix
+        agg_mat = intermediate;
+        tot_mat = horzcat(agg_mat, intermediate); % TODO THIS MIGHT NOT WORK, goal is to concatenate new intermediate matrix to existing matrix of linspace results
     end
-    final_p = transpose(MONDO); %should be a 3xN matrix holding all x, y, z vals with intermediate values
-    
+   
     R = [1, 0, 0; 0, 1, 0; 0, 0, 1; 0, 0, 0];
     
-    for Q = 1 : size(final_p)
-        super_final_p = cat(1, final_p, 1);
-        T = [R, super_final_p(Q)]; % 4x4 T matrix
-        a = T; 
+    [m,n] = size(tot_mat) %m is rows, n is columns, should be 3xN
+    for i=1:n
+        super_final_p = cat(1, tot_mat, 1);
+        T = [R, super_final_p(i)]; % 4x4 T matrix
+        a = T;
     end
 end
+
+
 
