497b54dca9a96a28771cc8a06ab4fc94aee0025c
Brita Hill <46576125+bhill56@users.noreply.github.com>
2020-03-09 17:39:31 -0700

    Added conversion files radians<->bits

---
+++ myMATLABFiles/Bit2RadA.m
@@ -0,0 +1,4 @@
+function [radian] = Bit2RadA(bit)
+
+	radian = bit * (300/1023) * (pi/180); 
+end 
---
+++ myMATLABFiles/Rad2BitA.m
@@ -0,0 +1,5 @@
+function [bit] = Rad2BitA(radian) 
+
+bit = radian * 180/(pi) (1023/300); 
+
+end 
